c83d29dc92fdd998caab51e997346d6c1f70e791
MALLESH C N <84223327+rohanworld@users.noreply.github.com>
2022-12-05 00:07:08 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,51 @@
+let submit=document.querySelector(".submit");
+let notesElem=document.querySelector('.notes');
+let title=document.querySelector("#text");
+let notes=JSON.parse(localStorage.getItem("notes"));
+
+var input = document.getElementById("text");
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addNotes();
+  }
+});
+if(notes){
+    notes.forEach(element => {
+        addNotes(element)
+    });
+}
+submit.addEventListener("click", (e)=>{
+    e.preventDefault();
+    addNotes()
+})
+function addNotes(obj) {
+    let card=document.createElement("div");
+    card.classList.add("card");
+    let titleval=title.value;
+    if(obj){
+        titleval=obj.title;
+    }
+    if(titleval){
+        card.innerHTML=`<h3>${titleval}</h3> <br> <br>
+                                  
+                             <button class="del">Delete</button>`;
+        notesElem.appendChild(card);
+        updateLs()
+    }
+    let del=card.querySelector(".del");
+    del.addEventListener('click', ()=>{
+        card.remove();
+        updateLs();
+    })
+}
+function updateLs() {
+    let card=document.querySelectorAll(".card");
+    let arr=[];
+    card.forEach(element => {
+        arr.push({
+            title:element.children[0].innerText,
+        })
+    });
+    localStorage.setItem("notes", JSON.stringify(arr));
+}
